d61f043d3bcc205dab46c39fce87a04d6e2d3dea
164424-RoshanKanwal <roshan.kanwal03@gmail.com>
2018-11-17 15:10:33 +0530

    Armstrong Number in Range

---
+++ AssignmentIntermediate/src/com/roshan/assignment1/ArmstrongInRange.java
@@ -0,0 +1,14 @@
+package com.roshan.assignment1;
+
+public class ArmstrongInRange {
+
+	public static void main(String[] args) {
+		for (int arms = 100; arms<=999; arms++){
+			
+			CheckArmstrongNumber.checkArmstrong(arms, 3);
+			
+		}
+
+	}
+
+}
